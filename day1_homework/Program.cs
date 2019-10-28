12344204f77a581619d08667eb7981dde1ff2e79
Ross Stapleton <rstapleton@spartaglobal.com>
2019-10-28 21:41:47 +0000

    created player and npc classes structure, add player and 1 ork

--- day1_homework/Program.cs
+++ day1_homework/Program.cs
@@ -3,14 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameComponents;
 
-namespace day1_homework
+namespace DayOneHomework
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Player player = new Player();
+
+            Ork ork1 = new Ork();
+            ork1.health = 10;
+
+            Console.WriteLine(ork1);
         }
     }
 }
-namespace  
+namespace GameComponents
+{
+    class Player
+    {
+        private double health;
+
+        private double mana;
+        private double magicDamage;
+
+        private double rageMeter;
+        private double oneHandDamage;
+    }
+    class NPC
+    {
+        private Player player;        
+    }
+    class Enemy : NPC
+    {
+        public double health;
+    }
+    class Ork : Enemy
+    {
+        private double rageMeter;
+        private double oneHandDamage;
+    }
+    class DarkWizard : Enemy
+    {
+        private double mana;
+        private double magicDamage;
+    }
+    class LightWizard : NPC
+    {
+        private double mana;
+        private double magicDamage;
+    }
+}
